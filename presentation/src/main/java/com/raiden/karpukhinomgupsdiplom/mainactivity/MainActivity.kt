0f1d3e48833f5b3684a96ed97c556a54bb133bac
Paul Karpukhin <ruberoid55@gmail.com>
2019-04-13 15:03:02 +0600

    Configured Navigation

---
+++ presentation/src/main/java/com/raiden/karpukhinomgupsdiplom/mainactivity/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.raiden.karpukhinomgupsdiplom.mainactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.navigateUp
+import com.raiden.karpukhinomgupsdiplom.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navController = findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, main_activity_drawer)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navigateUp(findNavController(this, R.id.nav_host_fragment), main_activity_drawer)
+    }
+}
