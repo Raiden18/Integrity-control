1a4db5dbceef2cfa64b69958ddf35f4a679d54ea
Paul Karpukhin <ruberoid55@gmail.com>
2019-04-20 22:40:46 +0600

    Added appearing animation for recycler view

--- presentation/src/main/java/com/raiden/karpukhinomgupsdiplom/screens/content/common/ContentFragment.kt
+++ presentation/src/main/java/com/raiden/karpukhinomgupsdiplom/screens/content/common/ContentFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.raiden.karpukhinomgupsdiplom.R
 import com.raiden.karpukhinomgupsdiplom.screens.content.common.adapter.ContentAdapter
@@ -57,11 +56,9 @@ abstract class ContentFragment : Fragment() {
     private fun initRecycler(fragment: View) {
         val recyclerView = fragment.applications_recycler_view
         val linearLayoutManager = LinearLayoutManager(context)
-        val itemDecorator = DividerItemDecoration(context, linearLayoutManager.orientation)
         recyclerView.apply {
             layoutManager = linearLayoutManager
             adapter = this@ContentFragment.adapter
-            addItemDecoration(itemDecorator)
         }
     }
 
